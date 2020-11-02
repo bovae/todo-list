2b05951522ff8881cb195bdb04faaf6f9abd5fd0
xbovae <egor.bova@dataart.com>
2020-11-02 13:08:53 +0200

    Structure refactoring

--- src/index.js
+++ src/index.js
@@ -1,32 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
-import ItemStatusFilter from './components/item-status-filter';
-
-import './index.css';
-
-const App = () => {
-
-    const todoData = [
-        {id: 1, label: 'Drink Coffee'},
-        {id: 2, label: 'Build Awesome App', important: true},
-        {id: 3, label: 'Have a lunch'}
-    ];
-
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3}/>
-            <div className="top-panel d-flex">
-                <SearchPanel/>
-                <ItemStatusFilter/>
-            </div>
-
-            <TodoList todos={todoData}/>
-        </div>
-    );
-};
+import App from "./components/app";
 
 ReactDOM.render(<App/>, document.getElementById('root'));
